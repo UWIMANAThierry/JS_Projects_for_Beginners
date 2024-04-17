8922d4746eadf4967c0cc05fe6a036619aa0a146
UWIMANAThierry <thierrybishop@gmail.com>
2024-04-17 09:47:23 +0200

    added new features

--- mini-calender/index.js
+++ mini-calender/index.js
@@ -4,5 +4,14 @@ const  dayNumEl = document.getElementById("day-number");
 const yearEl = document.getElementById("year");
 
 const date = new Date();
+const month = date.getMonth()
+monthNameEl.innerText = date.toLocaleDateString("en", {
+    month: "long"
+})
 
-console.log(date.getFullYear());
+dayNameEl.innerText = date.toLocaleDateString("en", {
+    weekday: "long"
+})
+
+dayNumEl.innerText = date.getDate()
+yearEl.innerText = date.getFullYear()
